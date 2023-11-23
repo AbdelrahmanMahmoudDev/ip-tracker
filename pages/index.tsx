c353a3877a2222a0aebe886848aa7fbba514d74d
AbdelRahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2023-11-23 14:17:08 +0200

    Reset project directory

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>IP Tracker</title>
+                <link rel="icon" href="/favicon.png" />
+            </Head>
+            <h1>Hello, World!</h1>
+        </>
+    )
+}

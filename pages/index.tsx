d62792716ee82f5039908081ee261f75e5725d16
AbdelRahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2023-11-24 22:35:56 +0200

    Added skeleton for header section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
 import Head from 'next/head';
 import {Layout} from '../components';
-
+import styles from "./index.module.css"
 export default function Home() {
     return (
         <>
             <Head>
                 <title>IP Tracker</title>
                 <link rel="icon" href="/favicon.png" />
+
             </Head>
-            <Layout>
-                <h1>Hello, World!</h1>
-            </Layout>
+            <header className={styles.header}>
+                <Layout>
+                    <section>
+                        <h1>ip address tracker</h1>
+                        <input type="text" id="ip" name="ip" required placeholder="Search for any IP address or domain"/>
+                    </section>
+                </Layout>
+            </header>
         </>
     )
 }

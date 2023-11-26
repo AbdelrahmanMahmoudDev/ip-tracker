448403981e2a67f76154b0ba08b95aa765fb8f82
AbdelRahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2023-11-26 09:22:04 +0200

    Completed header structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import {Layout} from '../components';
-import styles from "./index.module.css"
+import styles from "./index.module.css";
+import Image from "next/image";
 export default function Home() {
     return (
         <>
@@ -13,7 +14,12 @@ export default function Home() {
                 <Layout>
                     <section>
                         <h1>ip address tracker</h1>
-                        <input type="text" id="ip" name="ip" required placeholder="Search for any IP address or domain"/>
+                        <div className={styles.inputSection}>
+                            <input type="text" id="ip" name="ip" required placeholder="Search for any IP address or domain"/>
+                            <button>
+                                <Image width={11} height={14} src="images/icon-arrow.svg" alt="submit"/>
+                            </button>
+                        </div>
                     </section>
                 </Layout>
             </header>

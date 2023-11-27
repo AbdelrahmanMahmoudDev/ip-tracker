076a9d84f5c938c30bc83878bd63a18aff793afd
AbdelRahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2023-11-27 13:42:36 +0200

    Restructured project hierarchy

---
+++ pages/Home/Header/header.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import {Layout} from '../../../components';
+import styles from './header.module.css';
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <Layout>
+                <section>
+                    <h1>ip address tracker</h1>
+                    <div className={styles.inputSection}>
+                        <input type="text" id="ip" name="ip" required placeholder="Search for any IP address or domain"/>
+                        <button>
+                            <Image width={11} height={14} src="images/icon-arrow.svg" alt="submit"/>
+                        </button>
+                    </div>
+                </section>
+            </Layout>
+        </header>
+        )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,16 @@ import Head from 'next/head';
 import {Layout} from '../components';
 import styles from "./index.module.css";
 import Image from "next/image";
-export default function Home() {
+import {Header} from "./Home"
+
+export default function Composite() {
     return (
         <>
             <Head>
                 <title>IP Tracker</title>
                 <link rel="icon" href="/favicon.png" />
-
             </Head>
-            <header className={styles.header}>
-                <Layout>
-                    <section>
-                        <h1>ip address tracker</h1>
-                        <div className={styles.inputSection}>
-                            <input type="text" id="ip" name="ip" required placeholder="Search for any IP address or domain"/>
-                            <button>
-                                <Image width={11} height={14} src="images/icon-arrow.svg" alt="submit"/>
-                            </button>
-                        </div>
-                    </section>
-                </Layout>
-            </header>
+            <Header></Header>
         </>
     )
 }
